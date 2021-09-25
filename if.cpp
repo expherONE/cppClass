da4fa0cd28bee8b577e1c7ab45a8bb20065fad54
expherONE <olguin.mtz@outlook.com>
2021-09-24 19:08:31 -0500

    clase coki

---
+++ if.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std; 
+int main(){
+	int num;
+	cout<<"ingrese un numero";
+	cin>>num;
+	if(num==1){
+	cout <<"hola mundo";}
+	return 1;
+}
+
---
+++ multiplicacion.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+int numero1;
+int numero2;
+int numero3;
+int resultado;
+
+cout<<"ingrese el numero 1:"<<endl;
+cin>>numero1;
+
+cout<<"ingrese el numero 2:"<<endl;
+cin>>numero2;
+
+cout<<"ingrese el numero 3:"<<endl;
+cin>>numero3;
+
+resultado=numero1*numero2*numero3;
+cout<<"el resultado de la multiplicacion es:"<<endl;
+cout<< resultado;
+
+return 1;
+}
